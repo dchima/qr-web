c59f828594c633ed786e33f919cc26ab747c2029
dchima <dcnnad@gmail.com>
2020-12-23 17:23:14 +0100

    :cyclone: added qr

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import QRCode from 'qrcode.react';
 
 const AppContainer = styled.div`
   display: flex;
@@ -8,25 +9,62 @@ const AppContainer = styled.div`
   background-color: #03a9fc;
 `;
 
-const FormContainer = styled.div`
+const Content = styled.div`
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
   margin-top: 100px;
-  border: 1px solid red;
-  width: 20%;
+  // align-items: center;
+`;
+const FormContainer = styled.div`
+  // border: 1px solid red;
+  width: 100%;
   input {
-    width: 100%;
+    // width: 100%;
     padding: 12px 20px;
     text-align:center;
     border: 2px solid #000;
   }
 `;
 
+const QRContainer = styled.div`
+  border: 1px solid red;
+  margin-top: 50px;
+`;
+
 function App() {
+  const downloadQR = () => {
+    const canvas = document.getElementById("123456");
+    const pngUrl = canvas
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+    let downloadLink = document.createElement("a");
+    downloadLink.href = pngUrl;
+    downloadLink.download = "123456.png";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
   return (
     <AppContainer>
-      <FormContainer>
-        <input type='text' placeholder='Add text to convert'></input>
-      </FormContainer>
+      <Content>
+        <FormContainer>
+          <input type='text' placeholder='Add text to convert'></input>
+        </FormContainer>
 
+        <QRContainer>
+          <QRCode
+            id="123456"
+            value="123456"
+            size={290}
+            level={"H"}
+            includeMargin={true}
+          />
+        </QRContainer>
+
+        <a onClick={downloadQR}> Download QR </a>
+      </Content>
     </AppContainer>
   );
 }
